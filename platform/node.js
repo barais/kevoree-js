36301b3e36a89a66500da7a6fd518ee05999b0bf
Francois Fouquet <fouquet.f@gmail.com>
2013-08-19 10:58:18 +0200

    init nodeJS dev platform

--- platform/node.js
+++ platform/node.js
@@ -1,1 +1,25 @@
-//TODO
+var Class = require('./lib/Class.min.js');
+
+log = function(msg){
+    console.log(msg);
+}
+
+err = function(msg){
+    console.error(msg);
+}
+
+var Core = Class({
+    toString: 'Parent',
+    construct: function() {
+        console.log('Parent: Constructing');
+    },
+    destruct: function() {
+        console.log('Parent: Destructing');
+    },
+    doStuff: function() {
+        console.log('Parent: Doing stuff');
+    }
+});
+
+var kevoreeCore = new Core();
+kevoreeCore.doStuff();
