ed2dccf09858fb938e4e74c551b1959830b60fb3
Maxime Tricoire <leiko@braindead.fr>
2013-09-11 10:59:57 +0200

    minor changes

--- core/Bootstrapper.js
+++ core/Bootstrapper.js
@@ -44,40 +44,40 @@ module.exports = Class({
         if (nodeInstance != undefined && nodeInstance != null) {
             var deployUnits = nodeInstance.getTypeDefinition().getDeployUnits();
             if (deployUnits.size() > 0) {
-                console.log("Deploy Units:");
-                for (var i=0; i < deployUnits.size(); i++) {
-                    var packageName = deployUnits.get(i).getUnitName(),
-                        packageVersion = deployUnits.get(i).getVersion(),
-                        that = this;
+                // TODO OMG THIS IS UGLY
+                // but I'm not sure yet how to handle DU > ask François
+                var packageName = deployUnits.get(0).getUnitName(),
+                    packageVersion = deployUnits.get(0).getVersion(),
+                    that = this;
 
-                    npm.load({}, function (err) {
-                        if (err) {
-                            that.logger.log('Unable to load npm module');
+                npm.load({}, function (err) {
+                    if (err) {
+                        that.logger.log('Unable to load npm module');
+                        if (Util.callable(callback)) {
+                            callback.call(this, new Error('Bootstrap failure'));
+                            return;
+                        }
+                    }
+
+                    // load success
+                    npm.commands.install(that.modulesPath, [packageName+'@'+packageVersion], function (er) {
+                        if (er) {
+                            that.logger.log('npm failed to install package '+packageName+':'+packageVersion);
                             if (Util.callable(callback)) {
-                                callback.call(this, new Error('Bootstrap failure'));
+                                callback.call(this, new Error("Bootstrap failure"));
                                 return;
                             }
                         }
 
-                        // load success
-                        npm.commands.install(that.modulesPath, [packageName+'@'+packageVersion], function (er) {
-                            if (er) {
-                                that.logger.log('npm failed to install package '+packageName+':'+packageVersion);
-                                if (Util.callable(callback)) {
-                                    callback.call(this, new Error("Bootstrap failure"));
-                                    return;
-                                }
-                            }
-
-                            // install sucess
-                            if (Util.callable(callback)) {
-                                var NodeClass = require(path.resolve(that.modulesPath, 'node_modules', packageName));
-                                callback.call(this, null, NodeClass);
-                                return;
-                            }
-                        });
+                        // install sucess
+                        if (Util.callable(callback)) {
+                            var NodeClass = require(path.resolve(that.modulesPath, 'node_modules', packageName));
+                            callback.call(this, null, NodeClass);
+                            return;
+                        }
                     });
-                }
+                });
+
             } else {
                 if (Util.callable(callback)) {
                     callback.call(this, new Error("'"+nodeName+"' NodeType deploy units not found. Have you forgotten to merge nodetype library ?"));
