a735396f8df31003ad5b9aa564719062a2d06e4f
Maxime Tricoire <leiko@braindead.fr>
2013-10-09 16:37:10 +0200

    Grunt for libz = café

--- library/Gruntfile.js
+++ library/Gruntfile.js
@@ -1,50 +1,172 @@
 var fs      = require('fs'),
     path    = require('path'),
-    rimraf  = require('rimraf');
+    rimraf  = require('rimraf'),
+    async   = require('async');
 
 module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({});
 
+    // tasks
+    grunt.registerTask(
+        'clean',
+        "Runs 'rm -rf node_modules' in every module folder",
+        cleanTask
+    );
+    grunt.registerTask(
+        'install',
+        "Runs 'npm install' in every module folder",
+        installTask
+    );
+    grunt.registerTask(
+        'publish',
+        "Runs 'npm publish' in every module folder",
+        publishTask
+    );
+    grunt.registerTask(
+        'unpublish',
+        "Runs 'npm unpublish --force' in every module folder",
+        unpublishTask
+    );
+    grunt.registerTask(
+        'version',
+        "Runs 'npm version _argument_' (default argument is 'patch') in every module folder",
+        versionTask
+    );
 
-    grunt.registerTask('clean', "Run 'rm -rf node_modules' in every module folder", function() {
+    grunt.registerTask('foo', "Le foo task", function () {
+        var done = this.async();
+        setTimeout(function () {
+            console.log("FOO FOO FOO");
+            done();
+        }, 10000);
+    });
+
+    // task sets
+    grunt.registerTask('default', ['clean', 'install']);
+    grunt.registerTask('republish-all', ['clean', 'install', 'unpublish', 'publish']);
+    grunt.registerTask('publish-all', ['clean', 'install', 'publish']);
+    grunt.registerTask('patch-publish-all', ['clean', 'install', 'version:patch', 'publish']);
+    grunt.registerTask('minor-publish-all', ['clean', 'install', 'version:minor', 'publish']);
+    grunt.registerTask('major-publish-all', ['clean', 'install', 'version:major', 'publish']);
+};
+
+/**
+ * install task - runs 'npm install' in each module directory
+ */
+var installTask = function installTask() {
+    var done = this.async();
+
+    doInEachModule(done, function (modulePath, callback) {
         var exec = require('child_process').exec;
-        var cb = this.async();
-        // read current directory content
-        fs.readdir(__dirname, function (err, files) {
-            // for each file in directory
-            files.forEach(function (file) {
+        exec('npm install', {cwd: modulePath}, function(err, stdout, stderr) {
+            console.log(stdout);
+            callback(null);
+        });
+    });
+}
+
+/**
+ * clean task - runs 'rm -rf node_modules' in each module directory
+ */
+var cleanTask = function cleanTask() {
+    var done = this.async();
+
+    doInEachModule(done, function (modulePath, callback) {
+        var nodeModulesPath = path.resolve(modulePath, 'node_modules');
+        rimraf(nodeModulesPath, function (err) {
+            if (err) return callback(new Error("Unable to delete "+nodeModulesPath+" folder :/"));
+
+            // deletion succeed
+            console.log("Clean node_modules/ for %s", modulePath);
+            callback(null, nodeModulesPath);
+        });
+    });
+}
+
+/**
+ * publish task - runs 'npm publish' in each module directory
+ */
+var publishTask = function publishTask() {
+    var done = this.async();
+
+    doInEachModule(done, function (modulePath, callback) {
+        var exec = require('child_process').exec;
+        exec('npm publish', {cwd: modulePath}, function(err, stdout, stderr) {
+            console.log(stdout);
+            callback(null);
+        });
+    });
+}
+
+/**
+ * unpublish task - runs 'npm unpublish --force' in each module directory
+ */
+var unpublishTask = function unpublishTask() {
+    var done = this.async();
+
+    doInEachModule(done, function (modulePath, callback) {
+        var exec = require('child_process').exec;
+        exec('npm unpublish --force', {cwd: modulePath}, function(err, stdout, stderr) {
+            console.log(stdout);
+            callback(null);
+        });
+    });
+}
+
+/**
+ * version task - runs 'npm version [<newversion> | major | minor | patch | build]' in each module directory
+ */
+var versionTask = function versionTask(option) {
+    var done = this.async();
+
+    option = option || 'patch';
+
+    doInEachModule(done, function (modulePath, callback) {
+        var exec = require('child_process').exec;
+        exec('npm version '+option, {cwd: modulePath}, function(err, stdout, stderr) {
+            console.log(stdout);
+            callback(null);
+        });
+    });
+}
+
+/**
+ * Call command(modulePath, callback) for each module directory in __dirname
+ * - modulePath: /full/path/to/the/module
+ * - callback: function to callback when done (with a param => error)
+ * @param doneCallback
+ * @param command
+ */
+var doInEachModule = function doInEachModule(doneCallback, command) {
+    // read current directory content
+    fs.readdir(__dirname, function (err, files) {
+        if (err) {
+            console.error(err.message);
+            return doneCallback();
+        }
+
+        var moduleTasks = [];
+
+        // for each file in directory
+        files.forEach(function (file) {
+            moduleTasks.push(function (callback) {
                 // check if it is a directory
                 fs.lstat(file, function (err, stats) {
+                    if (err) return callback(err);
                     if (stats.isDirectory()) {
                         // it's a directory : which means that this is a kevoree library
-                        // remove node_modules folder
-                        var nodeModulesPath = path.resolve(__dirname, file, 'node_modules');
-                        rimraf(nodeModulesPath, function (err) {
-                            if (err) console.error("Unable to delete %s folder :/", nodeModulesPath);
-
-                            // deletion succeed => clean npm install
-                            exec('', {cwd: path.resolve(__dirname, file)}, function (err, stdout, stderr) {
-                                console.log(stdout);
-                                cb();
-                            });
-                        });
-                    }
+                        command(path.resolve(__dirname, file), callback);
+                    } else return callback();
                 });
             });
         });
-    });
 
-    grunt.registerTask('install', "Run 'npm install' in every module folder", function() {
-        var exec = require('child_process').exec;
-        var cb = this.async();
-        exec('echo "potato"', {cwd: '.'}, function(err, stdout, stderr) {
-            console.log(stdout);
-            cb();
+
+        async.parallel(moduleTasks, function (err, results) {
+            if (err) console.error(err.message);
+            doneCallback();
         });
     });
-
-    // default task
-    grunt.registerTask('default', ['clean', 'install']);
-};
+}
